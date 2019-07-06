ad1e0c30cf1179d885a2819cdf9998cfbd3a99d8
Soubhagya Mishra <trippywords0101@gmail.com>
2019-07-06 20:45:02 +0530

    Updates

---
+++ precache-manifest.8dfab30b8e3dd0a9cd55831a005fc74a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825c08ee7c7b5ad689eb335edab322b2",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "1421daeb79fcf1d3fdc8",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a682901baf6053cddb79",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1421daeb79fcf1d3fdc8",
+    "url": "/contactmanager/static/js/2.30d665e9.chunk.js"
+  },
+  {
+    "revision": "a682901baf6053cddb79",
+    "url": "/contactmanager/static/js/main.2eb7296b.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
